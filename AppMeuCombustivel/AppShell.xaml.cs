d4efe2769d81a3dbfc7f616577b84e2f2018f05e
Tiago Silva <tiago@tiago.blog.br>
2026-03-02 10:24:31 -0300

    Adicionar arquivos de projeto.

---
+++ AppMeuCombustivel/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppMeuCombustivel
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppMeuCombustivel/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace AppMeuCombustivel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            double preco_gasolina = Convert.ToDouble(gasolina.Text);
+            double preco_etanol = Convert.ToDouble(etanol.Text);
+            string msg = "";
+
+            if( preco_etanol > (preco_gasolina *0.7))
+            {
+                msg = "Compensa a gasolina";
+            } else
+            {
+                msg = "Compensa o etanol";
+            }
+            await DisplayAlertAsync("Resultado:", msg, "OK");
+        }
+    }
+}
